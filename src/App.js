e77b3b313d3d1a5a4475b7353425ce846b6c6b3e
youswim <youswim96@gmail.com>
2023-12-31 20:34:44 +0900

    조건부 렌더링으로 componentWillUnmount호출 확인

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 import LifecycleEx from "./LifecycleEx"
 
 function App() {
+  const [is_show, setIsShow] = React.useState(true);
   return (
     <div className="App">
-      <LifecycleEx />
+      {is_show? (<LifecycleEx />):null}
+      <button onClick={()=>{setIsShow(false);}}>없어져랏</button>
     </div>
   );
 }
